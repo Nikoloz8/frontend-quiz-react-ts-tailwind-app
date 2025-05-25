bed88ef04409a94cee106b9966c29b8da79b642e
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-25 17:18:19 +0400

    Commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout() {
 
     return (
         <>
-            <div className={`${!toggle ? "bg-[url('images/pattern-background-desktop-light.svg')]" : "bg-[url('images/pattern-background-desktop-dark.svg')]"} bg-cover bg-center bg-no-repeat w-[100vw] h-[100%] max-sm:p-[0_0_50px_0]! ${!toggle ? "bg-[#F4F6FA]" : "bg-[#313E51]"} flex items-center justify-center`}>
+            <div className={`${!toggle ? "bg-[url('images/pattern-background-desktop-light.svg')]" : "bg-[url('images/pattern-background-desktop-dark.svg')]"} bg-cover bg-center bg-no-repeat w-[100vw] h-[100%] ${!toggle ? "bg-[#F4F6FA]" : "bg-[#313E51]"} flex items-center justify-center`}>
                 <div className="w-[1200px] max-xl:w-[100%]! max-xl:p-[0_50px_0_50px]! max-sm:p-[0_30px_0_30px]! flex items-center justify-center">
                     <Outlet context={{ toggle, setToggle }} />
                 </div>
